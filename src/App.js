97c2ace5cb721759ff44cba4bd1e864697fae731
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2023-03-02 11:36:33 +0100

    Log in component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navigation from "./components/Navigation";
 import Main from "./components/Main";
+import LogIn from "./components/LogIn";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Main />
+      <LogIn />
     </div>
   );
 }
---
+++ src/components/LogIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LogIn() {
+    return (
+        <div>
+            <p>Logga in</p>
+        </div>
+    )
+}
+
+export default LogIn;
