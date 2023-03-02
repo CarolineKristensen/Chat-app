c32210d14ec83a3428e8923e85809d997e0d6f04
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2023-03-02 11:15:22 +0100

    Navigation created

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navigation() {
+    return (
+        <div>
+            <p>Det här är navigationen</p>
+        </div>
+    )
+}
+
+export default Navigation;
