79ac14ccb477eddf044fb8947a7cb1e07c96ca8a
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2023-03-03 16:19:12 +0100

    Tabs

--- src/components/logged-out/LoggedOutPage.js
+++ src/components/logged-out/LoggedOutPage.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    Container,
+    //Row,
+    //Col,
+    //Button,
+    Tab,
+    Tabs
+} from "react-bootstrap";
 
 function LoggedOutPage() {
+    const [key, setKey] = useState("login");
 
     return (
-        <>
+        <Container>
             <h1>Du är utloggad</h1>
-        </>
+            <Tabs
+             id="login"
+             activeKey={key}
+             onSelect={(k) => setKey(k)}
+             className="mb-3"
+            >
+                <Tab eventKey="login" title="Logga in"></Tab>
+                <Tab eventKey="register" title="Registrera"></Tab>
+            </Tabs>
+        </Container>
     )
 }
 
