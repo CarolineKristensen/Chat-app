34d73a76d5ff4e446661779eca3ddd2df73792fa
CarolineKristensen <caroline.kristensen@outlook.com>
2023-03-08 15:02:39 +0100

    Login
    Har börjat på en förstasida med enbart logga in som alternativ.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import Navigation from "./components/Navigation";
+import React from "react";
+//import Navigation from "./components/Navigation";
 import Main from "./components/Main";
-import Login from "./components/Login";
+//import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
+      
       <Main />
-      <Login />
+      
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./CSS/main.css";
+import Login from "./Login";
 
 function Main() {
     return (
         <div>
-            <p>Det här är main</p>
+            <h1 id="app-name">Welcome to Chat-App!</h1>
+            <div class="login-box">
+                <Login />
+            </div>
+            
         </div>
     )
 }
